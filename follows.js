9f6f3b96e036f79e75bdbecee75dcfc969fe1265
M <marwin_bartelink@hotmail.com>
2013-11-14 08:30:21 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 	var defaultKey		= 'fkJ3XLMe2elQccy94RmFBj5OyVykcwTtK1EBLb4USzlNZfzp', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [842570689], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '1week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= 'FF0000', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
